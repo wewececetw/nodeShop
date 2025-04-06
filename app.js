14aec8a1bf31409dfb611725b1deffae66bec705
barron <asd709380942@gamil.com>
2025-04-06 22:41:24 +0800

    feat: 引用css檔案

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const shopRouters = require("./routes/shop");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+// 讓伺服器提供 "public" 目錄下的靜態檔案，例如 CSS、JavaScript 和圖片。
+// 這使得 "public" 目錄中的檔案可以直接透過 HTTP 請求存取。
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRouters);
 
